d3eacdda29113ad7fa56c60b9ad699210abe6674
chrism <christopher.and.moran@gmail.com>
2020-12-23 14:40:39 -0800

    Updates README

--- examples/mm_three_dim.rs
+++ examples/mm_three_dim.rs
@@ -7,25 +7,23 @@ fn main() {
     //let input = open_rgb_image_and_convert_to_ndarray3("examples/ferris_ml.png").unwrap();
     let mut input = open_rgb_image_and_convert_to_ndarray3("examples/grand_canyon_trees.png").unwrap();
 
-
     let kernel_h = array![[1.0, 1.0, 1.0], [0.0, 0.0, 0.0], [-1.0, -1.0, -1.0]];
     let hp_hori = ConvHyperParam::default(kernel_h).stride((1, 1)).padding(0).build();
     let kernel_v = array![[-1.0, 0.0, 1.0], [-1.0, 0.0, 1.0], [-1.0, 0.0, 1.0]];
     let hp_vert = ConvHyperParam::default(kernel_v).stride((1, 1)).build();
 
-    //let output = mm_convolution_3d(input, &hp_hori).unwrap();
+    // let output = mm_convolution_3d(input, &hp_hori).unwrap();
     // let output = mm_convolution_3d(output, &hp_vert).unwrap();
 
-
     #[rustfmt::skip]
-    let kernel_d = array![
+    let kernel_edge = array![
         [-1.0, -1.0, -1.0],
         [-1.0, 8.0, -1.0],
         [-1.0, -1.0, -1.0]
     ];
-    let hp_diag = ConvHyperParam::default(kernel_d).stride((1,1)).padding(0).build();
+    let hp_edge = ConvHyperParam::default(kernel_edge).stride((1,1)).padding(0).build();
 
-    let output = single_mult_mm_convolution_3d(input.clone(), &hp_diag).unwrap();
+    let output = single_mult_mm_convolution_3d(input.clone(), &hp_edge).unwrap();
 
     let output_img = rgb_ndarray3_to_rgb_image(output);
     
@@ -57,7 +55,7 @@ fn main() {
         }
     }
     
-    std::thread::sleep(std::time::Duration::from_secs(5));
+    std::thread::sleep(std::time::Duration::from_secs(10));
     show_image::stop().unwrap();
 
 
